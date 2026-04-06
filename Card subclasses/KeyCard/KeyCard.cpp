d24622a29cd4f1adfd58b2d2ba3b03b2e7571a1d
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-06 15:13:18 +0930

    Implemented KeyCard skeleton class.

---
+++ Card subclasses/KeyCard/KeyCard.cpp
@@ -0,0 +1,17 @@
+#include "KeyCard.h"
+#include "Game.h"
+#include "Player.h"
+
+KeyCard::KeyCard(int value) : Card(value, CardType::Key) {
+
+}
+
+std::string KeyCard::str() const {
+	return "Key(" + std::to_string(getValue()) + ")";
+}
+
+void KeyCard::play(Game& game, Player& player) {
+}
+
+void KeyCard::willAddToBank(Game& game, Player& player) {
+}
