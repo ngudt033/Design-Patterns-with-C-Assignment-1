95f7d8de714bd5257a4701252ba55578baf556d8
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-07 04:19:45 +0930

    Incorporated HookCard ability.

--- DeadMansDraw A1/DeadMansDraw A1/HookCard.cpp
+++ DeadMansDraw A1/DeadMansDraw A1/HookCard.cpp
@@ -1,6 +1,7 @@
 #include "HookCard.h"
 #include "Game.h"
 #include "Player.h"
+#include <iostream>
 
 HookCard::HookCard(int value) : Card(value, CardType::Hook) {
 
@@ -11,5 +12,29 @@ std::string HookCard::str() const {
 }
 
 void HookCard::play(Game& game, Player& player) {
+	CardCollection& bank = player.getBank();
+
+	if (bank.empty()) {
+		std::cout << "Hook: No cards in the bank to play." << std::endl;
+		return;
+	}
+
+	int highestValue = 0;
+
+	for (int i = 1; i < (int)bank.size(); i++) {
+		if (bank[i]->getValue() > bank[highestValue]->getValue()) {
+			highestValue = i;
+		}
+	}
+	Card* hookedCard = player.removeBankCard(highestValue);
+
+	std::cout << "Hook: Played " << hookedCard->str() << " from the bank." << std::endl;
+	player.addCardToPlayArea(hookedCard);
+
+	if (player.isBust()) {
+		std::cout << player.getName() << " busted after playing " << hookedCard->str() << "!" << std::endl;
+	}
+
+	hookedCard->play(game, player);
 }
 
