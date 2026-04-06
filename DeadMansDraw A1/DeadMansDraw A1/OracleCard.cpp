dc133ba53b261267775832a9b38a34ff17900923
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-07 04:29:57 +0930

    Added Oracle ability.

--- DeadMansDraw A1/DeadMansDraw A1/OracleCard.cpp
+++ DeadMansDraw A1/DeadMansDraw A1/OracleCard.cpp
@@ -1,6 +1,7 @@
 #include "OracleCard.h"
 #include "Game.h"
 #include "Player.h"
+#include <iostream>
 
 OracleCard::OracleCard(int value) : Card(value, CardType::Oracle) {
 
@@ -11,4 +12,12 @@ std::string OracleCard::str() const {
 }
 
 void OracleCard::play(Game& game, Player& player) {
+	if (game.getDeck().empty()) {
+		std::cout << "Oracle: no cards left in the deck to peek at.";
+		return;
+	}
+
+	Card* topCard = game.getDeck().back();
+	std::cout << "Oracle: the top card of the deck is " << topCard->str() << std::endl;
+
 }
