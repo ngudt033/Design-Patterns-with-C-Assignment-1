b6464d4af551760a5cd121df342ef2aa17cb28e7
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-07 22:13:14 +0930

    Implementeds srand to actually randomise the name in conjuction with rand().

--- DeadMansDraw A1/DeadMansDraw A1/DeadMansDraw A1.cpp
+++ DeadMansDraw A1/DeadMansDraw A1/DeadMansDraw A1.cpp
@@ -1,8 +1,11 @@
 #include "Game.h"
 #include "game_title.h"
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
+	srand((unsigned)time(0));
 	std::cout << GAME_TITLE << std::endl;
 	Game game;
 	game.startGame();
