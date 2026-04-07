96129fc4b76644ae7928db2a480bbe747832115a
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-07 14:24:25 +0930

    Implemented Map ability.

--- DeadMansDraw A1/DeadMansDraw A1/MapCard.cpp
+++ DeadMansDraw A1/DeadMansDraw A1/MapCard.cpp
@@ -1,6 +1,7 @@
 #include "MapCard.h"
 #include "Game.h"
 #include "Player.h"
+#include <iostream>
 
 MapCard::MapCard(int value) : Card(value, CardType::Map) {
 
@@ -11,6 +12,53 @@ std::string MapCard::str() const {
 }
 
 void MapCard::play(Game& game, Player& player) {
+	CardCollection drawnCards;
+
+	for (int i = 0; i < 3; i++) {
+		if (game.getDiscardPile().empty()) {
+			return;
+		}
+		Card* drawnCard = game.getDiscardPile().back();
+		game.getDiscardPile().pop_back();
+		drawnCards.push_back(drawnCard);
+	}
+
+	if (drawnCards.empty()) {
+		std::cout << "Map: no cards in the discard pile to draw." << std::endl;
+		return;
+	}
+	
+	std::cout << "Map: drawn cards from discard pile: ";
+	for (int i = 0; i < (int)drawnCards.size(); i++) {
+		std::cout << drawnCards[i]->str() << " ";
+	}
+
+	int chosenIndex = 0;
+
+	for (int i = 1; i < (int)drawnCards.size(); i++) {
+		if (drawnCards[i]->getValue() > drawnCards[chosenIndex]->getValue()) {
+			chosenIndex = i;
+		}
+	}
+
+	Card* chosenCard = drawnCards[chosenIndex];
+
+	std::cout << "Map played: " << chosenCard->str() << " added to play area." << std::endl;
+	
+	player.addCardToPlayArea(chosenCard);
+
+	for (int i = 0; i < (int)drawnCards.size(); i++) {
+		if (i != chosenIndex) {
+			game.getDiscardPile().push_back(drawnCards[i]);
+		}
+	}
+
+	if (player.isBust()) {
+		std::cout << player.getName() << " busted after playing " << chosenCard->str() << "!" << std::endl;
+		return;
+	}
+
+	chosenCard->play(game, player);
 }
 
 
