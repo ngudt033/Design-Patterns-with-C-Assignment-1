70e90961ebd6c363b23c362cd54b7551adbcd263
ngudt033 <ngudt033@mymail.unisa.edu.au>
2026-04-07 15:04:07 +0930

    Implemented kraken ability

--- DeadMansDraw A1/DeadMansDraw A1/KrakenCard.cpp
+++ DeadMansDraw A1/DeadMansDraw A1/KrakenCard.cpp
@@ -1,6 +1,7 @@
 #include "KrakenCard.h"
 #include "Game.h"
 #include "Player.h"
+#include <iostream>
 
 KrakenCard::KrakenCard(int value) : Card(value, CardType::Kraken) {
 
@@ -11,4 +12,24 @@ std::string KrakenCard::str() const {
 }
 
 void KrakenCard::play(Game& game, Player& player) {
+	std::cout << "Kraken draws 3 cards.";
+
+	for (int i = 0; i < 3, i++) {
+		Card* extraCard = game.drawCard();
+
+		if (extraCard == nullptr) {
+			std::cout << "No more cards to draw." << std::endl;
+			return;
+		}
+		else {
+			std::cout << "Kraken draws " << extraCard->str() << std::endl;
+			bool bust = player.playCard(extraCard, game);
+
+			if (bust) {
+				std::cout << player.getName() << " busted after drawing " << extraCard->str() << "!" << std::endl;
+				return;
+			}
+			
+		}
+	}
 }
